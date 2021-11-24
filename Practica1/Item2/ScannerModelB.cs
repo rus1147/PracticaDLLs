e9c6d7457a61971be64d5abe609dbf21b6f0c9a7
Ruslan Sobol <rus1147@gmail.com>
2021-11-24 16:15:05 -0300

    Metodos TestScan, ImagesRes, Close y se eliminan comentarios en la clase Scanner

--- Practica1/Item2/ScannerModelB.cs
+++ Practica1/Item2/ScannerModelB.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ScannerBase;
 
 namespace ScannerModelB
 {
-    public class ScannerB
+    public class ScannerB: Scanner
     {
         internal bool estadoScannerB;
+        public ResolutionFormat _imageDpiB;
+
         public void Initialize()
         {
             estadoScannerB = true;
         }
-    }
+
+        public bool TestScan()
+        {
+            return estadoScannerB;
+        }
+
+        public void Close()
+        {
+            estadoScannerB &= false;
+        }
+
+        public ResolutionFormat  ImageRes{
+
+            get{
+                return randomDPI();
+            }   
+            set{
+                _imageDpiB = value;
+            }
+        }
+}
 }
--- Practica1/Scanner/Scanner.cs
+++ Practica1/Scanner/Scanner.cs
@@ -17,35 +17,6 @@ namespace ScannerBase
 
         public static string carpetaCheques = $"C:\\Users\\Ruslan\\Programacion\\academina BNA\\Practica1\\PracticaDLLs\\Practica1\\Img_Cheques\\";
 
-        
-        //public string CopyImage(ImgFormt_JPG_or_PNG format, ResolutionFormat reso, int quantity)
-        //{
-        //    string sourceDir = @carpetaCheques + reso.ToString();
-        //    string backupDir = DestinationDirectory + "\\" + reso.ToString();
-
-        //    string[] picList = Directory.GetFiles(sourceDir, "*.JPG");
-
-        //    string file = picList[quantity];
-        //    string fName = file.Substring(sourceDir.Length + 1);
-        //    string fNameNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
-
-        //    if (format == ImgFormt_JPG_or_PNG.JPG)
-        //    {
-        //        string result = Path.Combine(backupDir, fName);
-        //        File.Copy(Path.Combine(sourceDir, fName), result, true);
-        //        return result;
-        //    }
-        //    else if (format == ImgFormt_JPG_or_PNG.PNG)
-        //    {
-        //        string result = Path.Combine(backupDir, fNameNameWithoutExtension + ".PNG");
-        //        File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fNameNameWithoutExtension + ".PNG"), true);
-        //        return result;
-        //    }
-        //    else
-        //    {
-        //        return statusError;
-        //    }
-        //}
         public string CopyImage(ImgFormt_JPG_or_PNG format, ResolutionFormat reso)
         {
             string sourceDir = @carpetaCheques + reso.ToString();
@@ -77,16 +48,7 @@ namespace ScannerBase
                 return statusError;
             }
         }
-        //public string[] MultiScan(ImgFormt_JPG_or_PNG format, ResolutionFormat resolution, int quantity)
-        //{
-        //    List<string> lista = new List<string>();
-        //    for (int i = 0; i < quantity; i++)
-        //    {
-        //        lista.Add((CopyImage(format, resolution, i)));
-        //    }
-        //    String[] result = lista.ToArray();
-        //    return result;
-        //}
+       
         public ResolutionFormat randomDPI()
         {
             Random random = new Random();
@@ -122,41 +84,9 @@ namespace ScannerBase
             return _imageFormat;
         }
 
-        //public string DestinationDirectory
-        //{
-        //    get
-        //    {
-        //        return _destinationDirectory;
-        //    }
-
-        //    set
-        //    {
-        //        _destinationDirectory = value;
-        //    }
-        //}
-        //public void Stop()
-        //{
-        //    Console.WriteLine("Scanner has been interrupted");
-        //}
-        //public string GetStatus()
-        //{
-        //    Random random = new Random();
-        //    int statusNumber = random.Next(0, 5);
-
-        //    if (statusNumber < 5)
-        //    {
-        //        return statusOk;
-        //    }
-        //    else
-        //    {
-        //        return statusError;
-        //    }
-        //}
-
         public void DeleteImagenes(ResolutionFormat reso)
         {
-            string backupDir = _destinationDirectory + "\\" + reso.ToString();
-            //string[] picList = Directory.GetFiles(backupDir);
+            string backupDir = _destinationDirectory + "\\" + reso.ToString(); 
             DirectoryInfo dir = new DirectoryInfo(backupDir);
 
             foreach (FileInfo fi in dir.GetFiles())
@@ -164,11 +94,6 @@ namespace ScannerBase
                 fi.Delete();
             }
         }
-
-       
-
-
-
 
     }
 }
